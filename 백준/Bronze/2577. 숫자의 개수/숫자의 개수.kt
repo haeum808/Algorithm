6ba2e96b5d25645e7b5f92f42e67e1fdd5ccaa87
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-07-06 18:19:37 +0900

    [Bronze II] Title: 숫자의 개수, Time: 108 ms, Memory: 18640 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.kt
@@ -0,0 +1,23 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val result = IntArray(10)
+
+    val a = readLine().toInt()
+    val b = readLine().toInt()
+    val c = readLine().toInt()
+    val mul = a * b * c
+
+    mul.toString().forEach {
+        result[it.digitToInt()]++
+    }
+
+    bw.write(result.joinToString("\n"))
+
+    bw.flush()
+    bw.close()
+}
