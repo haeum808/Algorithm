a96a5d1590e6ffbc431f0a7d6bd04a968c3767ce
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-07-06 17:09:27 +0900

    [Bronze II] Title: 단어의 개수, Time: 164 ms, Memory: 20908 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.kt
@@ -0,0 +1,9 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val st = StringTokenizer(br.readLine())
+    println(st.countTokens())
+}
