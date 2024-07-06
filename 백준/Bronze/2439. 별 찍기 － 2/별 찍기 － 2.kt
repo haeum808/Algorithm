7df84228b1a6832dceba2fbd76c142161f1a910e
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-07-06 17:48:14 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 100 ms, Memory: 12500 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.kt
@@ -0,0 +1,27 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.StringTokenizer
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val st = StringTokenizer(readLine())
+
+    val n = st.nextToken().toInt()
+
+    repeat(n) { row ->
+        repeat(n - (row + 1)) {
+            bw.write(" ")
+        }
+
+        repeat(row + 1) {
+            bw.write("*")
+        }
+
+        bw.write("\n")
+    }
+
+    bw.flush()
+    bw.close()
+}
