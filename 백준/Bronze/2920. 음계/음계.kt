4693aa7eb150656785cf3ee9ba844255617b902c
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-07-07 13:59:20 +0900

    [Bronze II] Title: 음계, Time: 88 ms, Memory: 12240 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.kt
+++ 백준/Bronze/2920. 음계/음계.kt
@@ -2,15 +2,13 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.util.StringTokenizer
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val bw = BufferedWriter(OutputStreamWriter(System.out))
-    val st = StringTokenizer(readLine())
-    val targetNumbers = List(8) { st.nextToken() }
+    val targetNumbers = readLine()
 
-    val ascendingNumbers = listOf("1", "2", "3", "4", "5", "6", "7", "8")
-    val descendingNumbers = listOf("8", "7", "6", "5", "4", "3", "2", "1")
+    val ascendingNumbers = "1 2 3 4 5 6 7 8"
+    val descendingNumbers = "8 7 6 5 4 3 2 1"
 
     when(targetNumbers) {
         ascendingNumbers -> bw.write("ascending")
