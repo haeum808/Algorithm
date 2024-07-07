bbaee37d3e2a4542380f4f3938ea5ec79fc86bb8
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-07-07 16:04:03 +0900

    [Bronze V] Title: A×B, Time: 80 ms, Memory: 12308 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.kt
@@ -0,0 +1,17 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.StringTokenizer
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val st = StringTokenizer(readLine())
+    val a = st.nextToken().toInt()
+    val b = st.nextToken().toInt()
+
+    bw.write("${a * b}\n")
+    bw.flush()
+    bw.close()
+}
