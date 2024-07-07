b18f974da717c159b6d81d0fdc301d538ca3eda1
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-07-07 15:34:26 +0900

    [Bronze III] Title: 최소, 최대, Time: 420 ms, Memory: 117232 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.kt
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.kt
@@ -2,19 +2,24 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.util.*
+import java.util.StringTokenizer
+import kotlin.math.max
+import kotlin.math.min
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val n = readLine().toInt()
     val st = StringTokenizer(readLine())
-    val treeSet = TreeSet<Int>()
+    var minNumber = st.nextToken().toInt()
+    var maxNumber = minNumber
 
-    while (st.hasMoreTokens()) {
-        treeSet.add(st.nextToken().toInt())
+    for (i in 1 ..< n) {
+        val target = st.nextToken().toInt()
+        minNumber = min(minNumber, target)
+        maxNumber = max(maxNumber, target)
     }
 
-    bw.write("${treeSet.first()} ${treeSet.last()}")
+    bw.write("$minNumber $maxNumber")
 
     bw.flush()
     bw.close()
