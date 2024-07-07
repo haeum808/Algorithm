f7f452b7c268e2d52b2a185ed2bb3ee5df5bbbc7
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-07-07 16:07:03 +0900

    [Bronze V] Title: 아스키 코드, Time: 88 ms, Memory: 11976 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.kt
@@ -0,0 +1,12 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    bw.write("${readLine()[0].code}\n")
+    bw.flush()
+    bw.close()
+}
