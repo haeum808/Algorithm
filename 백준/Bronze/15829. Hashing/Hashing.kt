2e18c8916392e1805f2e8bfc18c500309772480a
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-07-09 13:34:46 +0900

    [Bronze II] Title: Hashing, Time: 92 ms, Memory: 12240 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.kt
+++ 백준/Bronze/15829. Hashing/Hashing.kt
@@ -2,20 +2,25 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import kotlin.math.pow
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
     val n = readLine().toInt()
     val str = readLine()
-    var result = 0
+    var result = 0L
 
     for (i in str.indices) {
-        result += (str[i].code - 96) * 31.0.pow(i).toInt()
+        var pow = 1L
+
+        repeat(i) {
+            pow = pow * 31 % 1234567891
+        }
+
+        result += (((str[i].code - 96) % 1234567891) * (pow % 1234567891)) % 1234567891
     }
 
-    bw.write("$result")
+    bw.write("${result % 1234567891}")
     bw.flush()
     bw.close()
 }
