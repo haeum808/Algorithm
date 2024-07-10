f16299c87f175dcc3297cd6205911fb93e944afe
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-07-10 16:53:38 +0900

    [Silver V] Title: 단어 정렬, Time: 300 ms, Memory: 35272 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.kt
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.kt
@@ -6,12 +6,26 @@ import java.io.OutputStreamWriter
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val n = readLine().toInt()
-    val strings = List<String>(n) { readLine() }
+    val treeSet = sortedSetOf<Word>()
 
-    strings.toSet().sortedWith(compareBy({ it.length }, { it })).forEach {
-        bw.write("${it}\n")
+    repeat(n) {
+        treeSet.add(Word(readLine()))
+    }
+
+    treeSet.forEach {
+        bw.write("${it.value}\n")
     }
 
     bw.flush()
     bw.close()
+}
+
+class Word(val value: String) : Comparable<Word> {
+    override fun compareTo(other: Word): Int {
+        return if (value.length == other.value.length) {
+            value.compareTo(other.value)
+        } else {
+            value.length - other.value.length
+        }
+    }
 }
