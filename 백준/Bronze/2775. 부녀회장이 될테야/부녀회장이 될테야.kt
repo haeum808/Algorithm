f487e1196c5a46c7b6a9a3ce2adddbac5050b4bc
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-07-10 10:45:58 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 96 ms, Memory: 12816 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.kt
@@ -0,0 +1,33 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val t = readLine().toInt()
+    val apart = Array(15) { IntArray(15) }
+
+    for (i in 1..14) {
+        apart[0][i] = i
+    }
+    for (i in 1..14) {
+        apart[i][0] = 1
+    }
+
+    for (i in 1..14) {
+        for (j in 1..14) {
+            apart[i][j] = (1..j).sumOf { apart[i- 1][it] }
+        }
+    }
+
+    repeat(t) {
+        val k = readLine().toInt()
+        val n = readLine().toInt()
+
+        bw.write("${apart[k][n]}\n")
+    }
+
+    bw.flush()
+    bw.close()
+}
