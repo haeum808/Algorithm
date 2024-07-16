1307ae06d47dd7cc7b1fc1227a038c58a7c634fa
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-07-16 05:32:25 -0700

    [Silver III] Title: 통계학, Time: 900 ms, Memory: 71080 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.kt
+++ 백준/Silver/2108. 통계학/통계학.kt
@@ -7,22 +7,35 @@ import kotlin.math.roundToInt
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val n = readLine().toInt()
-    val numbers = List(n) { readLine().toInt() }.sorted()
+    val numbers = mutableListOf<Int>()
+    val fq = IntArray(8001)
+
+    repeat(n) {
+        val input = readLine().toInt()
+
+        numbers.add(input)
+        fq[input + 4000] += 1
+    }
+
+    numbers.sort()
 
     bw.write("${numbers.average().roundToInt()}\n")
     bw.write("${numbers[n / 2]}\n")
 
-    val group = numbers.groupBy { it }
-    val max = group.maxOf { it.value.size }
+    val max = fq.max()
+    var value = Int.MIN_VALUE
 
-    group.filter { it.value.size == max }.let {
-        if (it.size == 1) {
-            bw.write("${it.keys.first()}\n")
-        } else {
-            bw.write("${it.keys.sorted()[1]}\n")
+    for (i in fq.indices) {
+
+        if (fq[i] == max && value == Int.MIN_VALUE) {
+            value = i - 4000
+        } else if (fq[i] == max) {
+            value = i - 4000
+            break
         }
     }
 
+    bw.write("$value\n")
     bw.write("${numbers[n - 1] - numbers[0]}")
     bw.flush()
     bw.close()
