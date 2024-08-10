cd2d4356d3035fa1ee65d9f4f0b130e892c6486b
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-08-10 07:12:58 -0700

    [Bronze II] Title: 바구니 뒤집기, Time: 136 ms, Memory: 21368 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.kt
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.kt
@@ -6,12 +6,22 @@ import java.io.OutputStreamWriter
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val (n, m) = readLine().split(" ").map { it.toInt() }
-    var balls = List(n) { it + 1 }
+    val balls = IntArray(n) { it + 1 }
+    val temp = IntArray(n)
 
     repeat(m) {
         val (i, j) = readLine().split(" ").map { it.toInt() - 1 }
 
-        balls = balls.slice(0..<i) + balls.slice(i..j).reversed() + balls.slice(j + 1..<n)
+        if (i != j) {
+            for (k in i..j) {
+                temp[k] = balls[k]
+            }
+
+            var cnt = 0
+            for (k in i..j) {
+                balls[k] = temp[j - cnt++]
+            }
+        }
     }
 
     balls.forEach {
