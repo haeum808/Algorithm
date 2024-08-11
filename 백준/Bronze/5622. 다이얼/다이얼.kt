ff7c4005042e467894599ee5a198f6e71923fc81
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-08-11 04:49:03 -0700

    [Bronze II] Title: 다이얼, Time: 104 ms, Memory: 15488 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.kt
@@ -0,0 +1,43 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val map = mapOf(
+        'A' to 2,
+        'B' to 2,
+        'C' to 2,
+        'D' to 3,
+        'E' to 3,
+        'F' to 3,
+        'G' to 4,
+        'H' to 4,
+        'I' to 4,
+        'J' to 5,
+        'K' to 5,
+        'L' to 5,
+        'M' to 6,
+        'N' to 6,
+        'O' to 6,
+        'P' to 7,
+        'Q' to 7,
+        'R' to 7,
+        'S' to 7,
+        'T' to 8,
+        'U' to 8,
+        'V' to 8,
+        'W' to 9,
+        'X' to 9,
+        'Y' to 9,
+        'Z' to 9,
+    )
+
+    val input = readLine()
+
+    bw.write("${input.map { map[it]!! + 1 }.sum()}")
+    bw.flush()
+    bw.close()
+    close()
+}
