e5a57b6ef0e410d490cadb021d514892c3eec3ee
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-08-11 04:40:41 -0700

    [Bronze III] Title: 그대로 출력하기, Time: 84 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.kt
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.kt
@@ -1,7 +1,15 @@
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+
 fun main() {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
     while (true) {
         val input = readlnOrNull() ?: break
 
-        println(input)
+        bw.write("${input}\n")
     }
+
+    bw.flush()
+    bw.close()
 }
