f3e2980df9ac7d33e4ec4ceaacbec0a2867a80bd
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-08-26 15:29:23 +0900

    [Silver III] Title: N과 M (1), Time: 368 ms, Memory: 53904 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.kt
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.kt
@@ -2,33 +2,32 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.util.Stack
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val (n, m) = readLine().split(" ").map { it.toInt() }
-    val numbers = ArrayList<Int>()
+    val numbers = ArrayList<Int>(n)
+    val visited = BooleanArray(n + 1)
 
     fun choose(curNum: Int) {
-        if (numbers.size != numbers.toSet().size) {
-            return
-        }
-
         if (curNum == m) {
             bw.write("${numbers.joinToString(" ")}\n")
             return
         }
 
         for (i in 1..n) {
-            numbers.add(i)
-            choose(curNum + 1)
-            numbers.removeLast()
+            if (!visited[i]) {
+                visited[i] = true
+                numbers.add(i)
+                choose(curNum + 1)
+                numbers.removeLast()
+                visited[i] = false
+            }
         }
     }
 
     choose(0)
-
-
+    
     bw.flush()
     bw.close()
     close()
