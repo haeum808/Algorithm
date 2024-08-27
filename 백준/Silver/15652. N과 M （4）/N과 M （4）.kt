da879aa8d4cc25e5622ea749a5bd71e332abb07f
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-08-27 15:05:39 +0900

    [Silver III] Title: N과 M (4), Time: 144 ms, Memory: 26660 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.kt
@@ -0,0 +1,30 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val (n, m) = readLine().split(" ").map { it.toInt() }
+
+    fun backtracking(depth: Int, answer: String) {
+        if (answer.length >= 4 && answer[answer.length - 2] < answer[answer.length - 4]) {
+            return
+        }
+
+        if (depth == m) {
+            bw.write("$answer\n")
+            return
+        }
+
+        for (number in 1..n) {
+            backtracking(depth + 1, "$answer$number ")
+        }
+    }
+
+    backtracking(0, "")
+
+    bw.flush()
+    bw.close()
+    close()
+}
