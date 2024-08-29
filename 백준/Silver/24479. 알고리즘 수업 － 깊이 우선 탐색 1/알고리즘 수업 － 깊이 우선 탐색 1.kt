0b7e026a4f5293e5792f5912962f0f9567355097
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-08-29 22:44:44 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 928 ms, Memory: 138784 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.kt
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.kt
@@ -1,46 +1,50 @@
-    import java.io.BufferedReader
-    import java.io.BufferedWriter
-    import java.io.InputStreamReader
-    import java.io.OutputStreamWriter
-    import java.util.LinkedList
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.PriorityQueue
 
-    fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-        val bw = BufferedWriter(OutputStreamWriter(System.out))
-        val (n, m, r) = readLine().split(" ").map { it.toInt() }
-        val graph = Array(n + 1) { LinkedList<Int>() }
-        val visited = BooleanArray(n + 1)
-        val result = IntArray(n + 1)
-        var count = 1
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val (n, m, r) = readLine().split(" ").map { it.toInt() }
+    val graph = Array(n + 1) { PriorityQueue<Int>() }
+    val result = IntArray(n + 1)
+    val visited = BooleanArray(n + 1)
+    var count = 1
 
-        repeat(m) {
-            val (a, b) = readLine().split(" ").map { it.toInt() }
+    repeat(m) {
+        val (a, b) = readLine().split(" ").map { it.toInt() }
 
-            graph[a].add(b)
-            graph[b].add(a)
-        }
+        graph[a].add(b)
+        graph[b].add(a)
+    }
 
-        for (i in 1..n) {
-            graph[i].sort()
-        }
+    fun dfs(start: Int) {
+        result[start] = count++
+        visited[start] = true
 
-        fun dfs(start: Int) {
-            result[start] = count++
-            visited[start] = true
-
-            for (neighbor in graph[start]) {
-                if (!visited[neighbor]) {
-                    dfs(neighbor)
+        while (graph[start].isNotEmpty()) {
+            graph[start].poll().let {
+                if (!visited[it]) {
+                    dfs(it)
                 }
             }
         }
 
-        dfs(r)
-
-        for (i in 1..n) {
-            bw.write("${result[i]}\n")
+        for (neighbor in graph[start]) {
+            if (!visited[neighbor]) {
+                dfs(neighbor)
+            }
         }
-
-        bw.flush()
-        bw.close()
-        close()
     }
+
+    dfs(r)
+
+    for (i in 1..n) {
+        bw.write("${result[i]}\n")
+    }
+
+    bw.flush()
+    bw.close()
+    close()
+}
