e4c3f22830a70c09bf857f760986dcfb63371653
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-09-03 19:01:31 +0900

    [Silver II] Title: N과 M (9), Time: 196 ms, Memory: 34244 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.kt
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.kt
@@ -2,24 +2,25 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.util.TreeSet
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val (n, m) = readLine().split(" ").map { it.toInt() }
     val numbers = readLine().split(" ").map { it.toInt() }.sorted()
     val isVisited = BooleanArray(n)
-    val results = mutableSetOf<String>()
 
     fun backtracking(depth: Int, result: String) {
         if (depth == m) {
-            results.add(result)
+            bw.write("$result\n")
             return
         }
 
+        var before = 0
+
         for (index in numbers.indices) {
-            if (!isVisited[index]) {
+            if (!isVisited[index] && before != numbers[index]) {
                 isVisited[index] = true
+                before = numbers[index]
                 backtracking(depth + 1, "${result}${numbers[index]} ")
                 isVisited[index] = false
             }
@@ -27,10 +28,6 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     }
 
     backtracking(0, "")
-
-    results.forEach {
-        bw.write("$it\n")
-    }
 
     bw.flush()
     bw.close()
