12de1695705f5b85b22fa050d7cd315ab08fbe3e
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-09-10 11:30:45 +0900

    [Silver II] Title: 최대 힙, Time: 280 ms, Memory: 28028 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.kt
@@ -0,0 +1,29 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.PriorityQueue
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val n = readLine().toInt()
+    val priorityQueue = PriorityQueue<Int>(reverseOrder())
+
+    repeat(n) {
+        val input = readLine().toInt()
+
+        if (input == 0) {
+            if (priorityQueue.isEmpty()) {
+                bw.write("0\n")
+            } else {
+                bw.write("${priorityQueue.poll()}\n")
+            }
+        } else {
+            priorityQueue.offer(input)
+        }
+    }
+
+    bw.flush()
+    bw.close()
+    close()
+}
