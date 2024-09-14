aa0284663ab4dbba24aa9ad837e8a813a42f17c9
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-09-15 00:21:44 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 452 ms, Memory: 293540 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.kt
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.kt
@@ -7,15 +7,19 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val input = readLine()
     val set = HashSet<String>()
+    var result = 0
 
     for (i in 1..input.length) {
         for (j in 0..input.length - i) {
             set.add(input.substring(j, j + i))
         }
+        result += set.size
+        set.clear()
     }
 
-    bw.write("${set.size}")
+    bw.write("$result")
     bw.flush()
     bw.close()
     close()
 }
+
