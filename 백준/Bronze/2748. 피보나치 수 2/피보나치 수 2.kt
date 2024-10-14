518eb2030fa4ec13f3a0e85cfa65a3e78718f902
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-10-14 21:18:17 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 76 ms, Memory: 12236 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.kt
@@ -0,0 +1,22 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val n = readLine().toInt()
+    val dp = LongArray(91)
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 1
+
+    for (i in 3..n) {
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+
+    bw.write("${dp[n]}")
+    bw.flush()
+    bw.close()
+    close()
+}
