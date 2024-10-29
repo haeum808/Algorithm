a5ad146ece105fecfc994ac2e8029f89b2a18052
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-10-29 16:55:13 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1924 ms, Memory: 230232 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.kt
@@ -0,0 +1,43 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.LinkedList
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val (n, m) = readLine().split(" ").map { it.toInt() }
+    val graph = Array(n + 1) { LinkedList<Int>() }
+    val visited = BooleanArray(n + 1)
+    var count = 0
+
+    fun dfs(x: Int) {
+        visited[x] = true
+
+        for (i in graph[x].indices) {
+            val next = graph[x][i]
+
+            if (visited[next].not()) {
+                dfs(next)
+            }
+        }
+    }
+
+    repeat(m) {
+        val (u, v) = readLine().split(" ").map { it.toInt() }
+        graph[u].add(v)
+        graph[v].add(u)
+    }
+
+    for (i in 1..n) {
+        if (visited[i].not()) {
+            count++
+            dfs(i)
+        }
+    }
+
+    bw.write("$count")
+    bw.flush()
+    bw.close()
+    close()
+}
