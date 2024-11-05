7df526354726d120ece884bcca66ec4b35484637
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-05 12:47:26 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 332 ms, Memory: 27236 KB -BaekjoonHub

---
+++ 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.kt
@@ -0,0 +1,38 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    var (n, k) = readLine().split(" ").map { it.toInt() }
+    val numbers = readLine().split(" ").map { it.toInt() }.toIntArray()
+
+    for (i in numbers.lastIndex downTo 1) {
+        var maxIndex = i
+
+        for (j in i - 1 downTo 0) {
+            if (numbers[maxIndex] < numbers[j]) {
+                maxIndex = j
+            }
+        }
+
+        if (i != maxIndex) {
+            val temp = numbers[maxIndex]
+            numbers[maxIndex] = numbers[i]
+            numbers[i] = temp
+            k--
+
+            if (k == 0) {
+                bw.write("${numbers[maxIndex]} ${numbers[i]}")
+                break
+            }
+        }
+    }
+
+    if (k != 0) bw.write("-1")
+
+    bw.flush()
+    bw.close()
+    close()
+}
