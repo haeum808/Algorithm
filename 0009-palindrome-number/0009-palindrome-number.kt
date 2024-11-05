d14a283394f3cb9bb5623e1217f89de8cfa1d7d8
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-06 00:03:55 +0900

    Time: 5 ms (100%), Space: 36.7 MB (70.53%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.kt
+++ 0009-palindrome-number/0009-palindrome-number.kt
@@ -1,5 +1,17 @@
 class Solution {
     fun isPalindrome(x: Int): Boolean {
-        return "$x".reversed() == "$x"
+        if (x == 0) return true
+        if (x < 0) return false
+        if (x.mod(10) == 0) return false
+        
+        var first = x
+        var revert = 0
+
+        while (first > revert) {
+            revert = revert * 10 + first.mod(10)
+            first /= 10
+        }
+
+        return first == revert || first == revert / 10
     }
 }
