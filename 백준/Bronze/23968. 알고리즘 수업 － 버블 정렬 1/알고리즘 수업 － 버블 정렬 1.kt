74bbb3ff9ff3d8f6cb9610e4c4440dc616f5f4f8
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-05 15:58:01 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 1, Time: 364 ms, Memory: 24404 KB -BaekjoonHub

---
+++ 백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.kt
@@ -0,0 +1,36 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    var (n, k) = readLine().split(" ").map { it.toInt() }
+    val numbers = readLine().split(" ").map { it.toInt() }.toIntArray()
+
+    for (last in n - 1 downTo 1) {
+        for (i in 0..<last) {
+            if (numbers[i] > numbers[i + 1]) {
+                val temp = numbers[i + 1]
+                numbers[i + 1] = numbers[i]
+                numbers[i] = temp
+                k--
+            }
+
+            if (k == 0) {
+                bw.write("${numbers[i]} ${numbers[i + 1]}")
+                break
+            }
+        }
+
+        if (k == 0) break
+    }
+
+    if (k != 0) {
+        bw.write("-1")
+    }
+
+    bw.flush()
+    bw.close()
+    close()
+}
