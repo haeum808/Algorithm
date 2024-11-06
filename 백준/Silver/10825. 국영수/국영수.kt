b8ea23873afe97f7f538a8d5c8b1d638f2e158a1
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-06 21:42:59 +0900

    [Silver IV] Title: 국영수, Time: 632 ms, Memory: 74556 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.kt
+++ 백준/Silver/10825. 국영수/국영수.kt
@@ -8,21 +8,15 @@ data class Student(
     val name: String,
     val korean: Int,
     val english: Int,
-    val math: Int,
-): Comparable<Student> {
-    override fun compareTo(other: Student): Int {
-        return if (korean == other.korean && english == other.english && math == other.math) {
-            name.compareTo(other.name)
-        } else if (korean == other.korean && english == other.english) {
-            other.math - math
-        } else if (korean == other.korean) {
-            english - other.english
-        } else {
-            other.korean - korean
-        }
+    val math: Int
+) : Comparable<Student> {
+    override fun compareTo(other: Student): Int = when {
+        korean != other.korean -> other.korean - korean
+        english != other.english -> english - other.english
+        math != other.math -> other.math - math
+        else -> name.compareTo(other.name)
     }
 }
-
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val n = readLine().toInt()
