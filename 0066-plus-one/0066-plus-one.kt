a94420ba15a591772aa26d974635d71d0cd955f0
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-06 11:58:23 +0900

    Time: 8 ms (49.17%), Space: 38.5 MB (8.42%) - LeetHub

--- 0066-plus-one/0066-plus-one.kt
+++ 0066-plus-one/0066-plus-one.kt
@@ -1,19 +1,21 @@
 class Solution {
     fun plusOne(digits: IntArray): IntArray {
-        val plusOneDigits = IntArray(digits.size + 1)
-        var over = 1
+        var isCarry = true
+        var index = digits.size - 1
 
-        for (i in digits.size - 1 downTo 0) {
-            plusOneDigits[i + 1] = (digits[i] + over) % 10
-
-            over = (digits[i] + over) / 10
+        while (isCarry && index >= 0) {
+            digits[index] = (digits[index] + 1) % 10
+            isCarry = digits[index] == 0
+            index--
         }
-
-        return if (over == 1) {
-            plusOneDigits[0] = 1
-            plusOneDigits
+        
+        return if (isCarry) {
+            IntArray(digits.size + 1).apply {
+                this[0] = 1
+                digits.copyInto(this, 1)
+            }
         } else {
-            plusOneDigits.slice(1..digits.size).toIntArray()
+            digits
         }
     }
 }
