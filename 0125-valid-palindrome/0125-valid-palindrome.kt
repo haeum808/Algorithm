c63ae3eee73becad034916e34c24ac1d62b0b470
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-07 18:53:32 +0900

    Time: 3 ms (100%), Space: 37 MB (84.81%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.kt
+++ 0125-valid-palindrome/0125-valid-palindrome.kt
@@ -1,16 +1,18 @@
 class Solution {
     fun isPalindrome(s: String): Boolean {
-        var newS = s.lowercase().filter { it in 'a'..'z' || it in '0'..'9' }
         var start = 0
-        var end = newS.length - 1
+        var end = s.length - 1
 
         while (start < end) {
-            if (newS[start] == newS[end]) {
-                start++
-                end--
-            } else {
+            while (start < end && s[start].isLetterOrDigit().not()) start++
+            while (start < end && s[end].isLetterOrDigit().not()) end--
+
+            if (s[start].lowercaseChar() != s[end].lowercaseChar()) {
                 return false
             }
+
+            start++
+            end--
         }
 
         return true
