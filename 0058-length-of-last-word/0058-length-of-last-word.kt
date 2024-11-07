e3039a495fcb6fe5a2c63b176c75a352366e586f
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-07 18:22:11 +0900

    Time: 0 ms (100%), Space: 34.8 MB (78.15%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.kt
+++ 0058-length-of-last-word/0058-length-of-last-word.kt
@@ -1,5 +1,17 @@
 class Solution {
     fun lengthOfLastWord(s: String): Int {
-        return s.trim().split(" ").last().length
+        var count = 0
+        var findNotBlank = false
+
+        for (i in s.length - 1 downTo 0) {
+            if (findNotBlank && s[i] == ' ') break
+
+            if (s[i] != ' ') {
+                findNotBlank = true
+                count++
+            }
+        }
+
+        return count
     }
 }
