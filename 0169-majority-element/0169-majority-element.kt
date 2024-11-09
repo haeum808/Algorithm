0884448d639012206b6ffef51936fd06b00ba4d0
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-09 18:47:24 +0900

    Time: 28 ms (16.14%), Space: 45.9 MB (12.9%) - LeetHub

--- 0169-majority-element/0169-majority-element.kt
+++ 0169-majority-element/0169-majority-element.kt
@@ -1,22 +1,26 @@
 class Solution {
-    
     fun majorityElement(nums: IntArray): Int {
-        nums.sort()
-        var prevNum = Int.MAX_VALUE
-        var targetSize = nums.size / 2
-        var count = 0
-
-        for (index in nums.indices) {
-            if (prevNum == nums[index]) {
-                count++
+    var hashMapEl: HashMap<Int, Int> = HashMap<Int, Int>()
+    var maxValue = 0
+    var key: Int = 0
+//Check that array is less that 3
+    if (nums.size == 1 || nums.size == 2) {
+        key = nums[0]
+    } else {
+        for (i in nums) {
+            var count = 1
+//If the element is found in the array first time add it in hash map as key and give the value one, because it find only once. Else increment value*
+            if (!hashMapEl.containsKey(i)) {
+                hashMapEl[i] = 1
             } else {
-                prevNum = nums[index]
-                count = 0
+                hashMapEl[i] = (hashMapEl[i] ?: 1) + 1
             }
-
-            if (count >= targetSize) return nums[index]
         }
-
-        return -1
+// Find the max element in hash map's values, it will be the the largest number of repetitions in the array */
+        maxValue = hashMapEl.values.maxOrNull()!!
+// Find the key of this value and it we be the majority element 
+        key = hashMapEl.entries.find { it.value == maxValue }?.key!!
+    }
+    return key
     }
 }
