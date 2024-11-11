e6db18cfd11787b6c82a2fd3fe52c7dd1d7fe929
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-11 12:50:58 +0900

    Time: 9 ms (25.27%), Space: 37.9 MB (14.79%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
+        for(i in 0..<n) {
+            nums1[i + m] = nums2[i]
+        }
+
+        nums1.sort()
+    }
+}
