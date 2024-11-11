efa43e59f9f2744c0ef5f8fd5807e8f80f5bf322
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-11 17:44:40 +0900

    Time: 0 ms (100%), Space: 35.4 MB (52.18%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.kt
+++ 0088-merge-sorted-array/0088-merge-sorted-array.kt
@@ -1,9 +1,27 @@
 class Solution {
     fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
-        for(i in 0..<n) {
-            nums1[i + m] = nums2[i]
+        val answer = IntArray(m + n)
+        var index = 0
+        var i = 0
+        var j = 0
+
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j])
+                answer[index++] = nums1[i++]
+            else
+                answer[index++] = nums2[j++]   
         }
 
-        nums1.sort()
+        while (i < m) {
+            answer[index++] = nums1[i++]
+        }
+
+        while (j < n) {
+            answer[index++] = nums2[j++]
+        }
+
+        for (i in 0..<(m + n)) {
+            nums1[i] = answer[i]
+        }
     }
 }
