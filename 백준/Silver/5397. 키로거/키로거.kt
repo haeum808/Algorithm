8a91603245590fe7181cc7409c9f9f248777b4e7
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-13 19:28:28 +0900

    [Silver II] Title: 키로거, Time: 1116 ms, Memory: 141536 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.kt
+++ 백준/Silver/5397. 키로거/키로거.kt
@@ -2,27 +2,27 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.util.LinkedList
+import java.util.Stack
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val t = readLine().toInt()
 
     repeat(t) {
-        val result = LinkedList<Char>()
+        val leftStack = Stack<Char>()
+        val rightStack = Stack<Char>()
         val input = readLine()
-        var index = 0
 
         for (char in input) {
             when (char) {
-                '<' -> if (index != 0) index--
-                '>' -> if (index != result.size) index++
-                '-' -> if (index != 0 && result.isNotEmpty()) result.removeAt(--index)
-                else -> result.add(index++, char)
+                '<' -> if (leftStack.isNotEmpty()) rightStack.push(leftStack.pop())
+                '>' -> if (rightStack.isNotEmpty()) leftStack.push(rightStack.pop())
+                '-' -> if (leftStack.isNotEmpty()) leftStack.pop()
+                else -> leftStack.push(char)
             }
         }
 
-        bw.write("${result.joinToString("")}\n")
+        bw.write("${leftStack.joinToString("")}${rightStack.reversed().joinToString("")}\n")
     }
 
     bw.flush()
