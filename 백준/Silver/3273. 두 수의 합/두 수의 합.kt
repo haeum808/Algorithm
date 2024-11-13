3437d77e8eca000d0a5753a2dc5a6ce80363c3eb
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-13 13:42:14 +0900

    [Silver III] Title: 두 수의 합, Time: 320 ms, Memory: 43464 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.kt
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.kt
@@ -4,28 +4,20 @@ import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    val bw = BufferedWriter(OutputStreamWriter(System.out))
-    val n = readLine().toInt()
-    val numbers = readLine().split(" ").map { it.toInt() }.sorted()
-    val x = readLine().toInt()
-    var i = 0
-    var j = numbers.lastIndex
-    var count = 0
+  val bw = BufferedWriter(OutputStreamWriter(System.out))
+  val n = readLine().toInt()
+  val map = HashMap<Int, Boolean>()
+  val numbers = readLine().split(" ").map { it.toInt() }
+  val x = readLine().toInt()
+  var count = 0
 
-    while (i < j) {
-        if (numbers[i] + numbers[j] == x) {
-            count++
-            i++
-            j--
-        } else if (numbers[i] + numbers[j] < x) {
-            i++
-        } else {
-            j--
-        }
-    }
+  for (index in 0..<n) {
+    if (map[x - numbers[index]] != null) count++
+    else map[numbers[index]] = true
+  }
 
-    bw.write("$count")
-    bw.flush()
-    bw.close()
-    close()
+  bw.write("$count")
+  bw.flush()
+  bw.close()
+  close()
 }
