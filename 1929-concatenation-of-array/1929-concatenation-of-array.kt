558f4fdaee1be94faa20bd295b82121c670853a6
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-15 22:11:15 +0900

    Time: 1 ms (100%), Space: 39.2 MB (34.82%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.kt
+++ 1929-concatenation-of-array/1929-concatenation-of-array.kt
@@ -1,5 +1,12 @@
 class Solution {
     fun getConcatenation(nums: IntArray): IntArray {
-        return nums + nums
+        val ans = IntArray(nums.size * 2)
+
+        for (i in nums.indices) {
+            ans[i] = nums[i]
+            ans[i + nums.size] = nums[i]
+        }
+
+        return ans
     }
 }
