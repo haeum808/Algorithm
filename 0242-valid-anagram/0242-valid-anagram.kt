ae4c819f4ef5ccc357c767b260c0342745097cb1
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-16 18:15:12 +0900

    Time: 22 ms (62.66%), Space: 37.6 MB (54.45%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun isAnagram(s: String, t: String): Boolean {
+        if (t.length < s.length) return false
+
+        val map = HashMap<Char, Int>()
+
+        for (char in s) {
+            map[char] = map.getOrDefault(char, 0) + 1
+        }
+
+        for (char in t) {
+            if (map[char] == null) return false
+
+            map[char] = map[char]!! - 1
+
+            if (map[char]!! < 0) return false
+        }
+
+        return true
+    }
+}
