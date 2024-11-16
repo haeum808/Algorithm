3f004ef5f531fff6d1f93ccf20edaec8e657b723
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-17 00:53:47 +0900

    Time: 0 ms (100%), Space: 33.9 MB (14.19%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.kt
+++ 0191-number-of-1-bits/0191-number-of-1-bits.kt
@@ -1,13 +1,5 @@
 class Solution {
     fun hammingWeight(n: Int): Int {
-        var copyN = n
-        var result = 0
-
-        while (copyN > 0) {
-            if (copyN % 2 != 0) result++
-            copyN /= 2 
-        }
-
-        return result
+        return n.countOneBits()
     }
 }
