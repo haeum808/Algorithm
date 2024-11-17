146c8575d92bf5c6fc545f66a58b260572dd5677
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-17 16:46:05 +0900

    Time: 34 ms (13.14%), Space: 37.3 MB (51.77%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.kt
@@ -0,0 +1,29 @@
+class Solution {
+    fun isIsomorphic(s: String, t: String): Boolean {
+        val map1 = HashMap<Char, Int>()
+        val map2 = HashMap<Char, Int>()
+        var index = 0
+
+        while (index < s.length) {
+            if (map1[s[index]] == null && map2[t[index]] == null) {
+                map1[s[index]] = index
+                map2[t[index]] = index
+            } else if (map1[s[index]] == null && map2[t[index]] != null) {
+                return false
+            } else if (map1[s[index]] != null && map2[t[index]] == null) {
+                return false
+            }
+
+            if (map1[s[index]] != map2[t[index]]) return false
+
+            while(map1[s[index]] != map2[t[index]]) {
+                index++
+                continue    
+            }
+
+            index++
+        }
+
+        return true
+    }
+}
