0ced7821ae9b567949fa306ea65589a50e8f2ab2
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-17 10:34:03 +0900

    Time: 7 ms (67.53%), Space: 36.8 MB (67.85%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.kt
+++ 0009-palindrome-number/0009-palindrome-number.kt
@@ -1,5 +1,12 @@
 class Solution {
     fun isPalindrome(x: Int): Boolean {
-        return "$x" == "$x".reversed()
+        if (x < 0) return false
+        val xString = "$x"
+
+        for (index in 0..<xString.length) {
+            if (xString[index] != xString[xString.length - index - 1]) return false
+        }
+ 
+        return true
     }
 }
