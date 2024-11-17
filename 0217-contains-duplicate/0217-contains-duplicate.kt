cd88968802c2f1a5c98393ede264aced6faa954c
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-17 22:13:50 +0900

    Time: 18 ms (76.1%), Space: 50.9 MB (59.13%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.kt
+++ 0217-contains-duplicate/0217-contains-duplicate.kt
@@ -1,12 +1,24 @@
 class Solution {
     fun containsDuplicate(nums: IntArray): Boolean {
-        return nums.toSet().size != nums.size
+        val set = hashSetOf<Int>()
+
+        for (num in nums) {
+            if (set.contains(num)) return true
+            set.add(num)
+        }
+
+        return false
     }
 }
 
-/*
+/* first think
 1. nums -> set
 2. nums.size , set.size
 2.1 -> same -> no duplicate
 2.2 -> not same -> there duplicate
+ */
+
+/* second think (not use toSet())
+1. set -> check occured 
+2. until if i find the num in set return false
  */
