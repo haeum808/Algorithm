954d0c08896fc2eb677552da235dc2f23694b3e1
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-20 13:43:24 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 108 ms, Memory: 12948 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.kt
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.kt
@@ -2,15 +2,25 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import java.util.StringTokenizer
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val n = readLine().toInt()
-    val numbers = readLine().split(" ").map { it.toInt() }
-    val dp = IntArray(n) { numbers[it] }
-    var result = numbers.max()
+    val st = StringTokenizer(readLine())
+    val numbers = IntArray(n)
+    var result = 0
+    var index = 0
 
-    for (i in 0..<n) {
+    while (st.hasMoreTokens()) {
+        numbers[index] = st.nextToken().toInt()
+        result = maxOf(result, numbers[index])
+        index++
+    }
+
+    val dp = IntArray(n) { numbers[it] }
+
+    for (i in 1..<n) {
         for (j in 0..<i) {
             if (numbers[i] > numbers[j]) {
                 dp[i] = maxOf(dp[i], dp[j] + numbers[i])
