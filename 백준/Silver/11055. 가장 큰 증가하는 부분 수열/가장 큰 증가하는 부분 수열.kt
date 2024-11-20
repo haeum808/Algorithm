7bbc7055ae071005c59e2a76d78ad3ae6b558179
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-11-20 11:48:15 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 184 ms, Memory: 21820 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.kt
@@ -0,0 +1,27 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val n = readLine().toInt()
+    val numbers = readLine().split(" ").map { it.toInt() }
+    val dp = IntArray(n)
+    var result = numbers.max()
+
+    for (i in 0..<n) {
+        dp[i] = numbers[i]
+        for (j in 0..<i) {
+            if (numbers[i] > numbers[j]) {
+                dp[i] = maxOf(dp[i], dp[j] + numbers[i])
+                result = maxOf(result, dp[i])
+            }
+        }
+    }
+
+    bw.write("$result")
+    bw.flush()
+    bw.close()
+    close()
+}
