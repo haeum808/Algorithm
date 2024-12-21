6464d66e7f69d991dcdcd530de5c87c81e2da444
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-12-21 23:24:20 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 564 ms, Memory: 96540 KB -BaekjoonHub

---
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val n = readln().toInt()
+    val sb = StringBuilder()
+
+    repeat(n) {
+        val input = readln()
+
+        sb.append("${input.split(" ").joinToString(" ") { it.reversed() }}\n")
+    }
+
+    print(sb)
+}
