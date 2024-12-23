894027d55ac80239b6b1649c553f21efef3b0b54
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-12-24 07:29:53 +0900

    [Bronze I] Title: N번째 큰 수, Time: 228 ms, Memory: 27148 KB -BaekjoonHub

---
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val t = readln().toInt()
+    val sb = StringBuilder()
+
+    repeat(t) {
+        val numbers = readln().split(" ").map { it.toInt() }.sortedDescending()
+
+        sb.append("${numbers[2]}\n")
+    }
+
+    print(sb)
+}
